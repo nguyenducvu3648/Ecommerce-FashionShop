ebaab2415a2ee6bf539476017497432032bcfb86
ducvu <vu99550@donga.edu.vn>
2024-10-13 16:11:04 +0700

    update swagger

--- src/main/java/com/demo_crud/demo/Configuration/SwaggerConfig.java
+++ src/main/java/com/demo_crud/demo/Configuration/SwaggerConfig.java
@@ -1,21 +1,19 @@
 package com.demo_crud.demo.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Your API Title")
+                        .description("Your API Description")
+                        .version("1.0"));
     }
 }
+//http://localhost:8080/identity/swagger-ui/index.html
--- src/main/java/com/demo_crud/demo/controller/UserController.java
+++ src/main/java/com/demo_crud/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.demo_crud.demo.dto.request.UserCreationRequest;
 import com.demo_crud.demo.dto.request.UserUpdateRequest;
 import com.demo_crud.demo.dto.response.UserResponse;
 import com.demo_crud.demo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -22,9 +24,11 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequestMapping("/users")
+@Tag(name = "Quản lý User")
 public class UserController {
     UserService userService;
 
+    @Operation(summary = "tat ca user")
     @GetMapping("")
     @PreAuthorize("hasRole('ADMIN')")//kiem tra vai tro
 //    @PreAuthorize("hasAuthority('APROVE_POST')")    kiem tra quyen truoc khi chay
@@ -34,12 +38,14 @@ public class UserController {
         authentication.getAuthorities().forEach(grantedAuthority -> log.info(grantedAuthority.getAuthority()));
         return userService.getAllUsers();
     }
+    @Operation(summary = "dang ki")
     @PostMapping("/sign-up")
     ApiResponse<UserResponse> createUser(@RequestBody @Valid UserCreationRequest request) {
         ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
         apiResponse.setData(userService.createUser(request));
         return apiResponse;
     }
+    @Operation(summary = "tim kiem user")
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     public ApiResponse<UserResponse> getUserById(@PathVariable String id) {
@@ -47,6 +53,7 @@ public class UserController {
     }
     //@PreAuthorize kiem tra truoc khi chay funcion
     //@PostAuthorize chay funcion roi kiem tra neu dung thi tra ket qua
+    @Operation(summary = "cap nhat user")
     @PutMapping("/{id}")
     @PostAuthorize("returnObject.data.username == authentication.name")
     ApiResponse<UserResponse> updateUser(@PathVariable String id, @RequestBody @Valid UserUpdateRequest request) {
@@ -54,12 +61,14 @@ public class UserController {
         apiResponse.setData(userService.updateUser(id, request));
         return apiResponse;
     }
+    @Operation(summary = "xoa user")
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
     String deleteUser(@PathVariable String id) {
          userService.deleteUser(UUID.fromString(id));
          return "User deleted";
     }
+    @Operation(summary = "thong tin user")
     @GetMapping("/myInfo")
     @PostAuthorize("returnObject.data.username == authentication.name")//user chi co the lay thong tin cua chinh minh ma thoi
     ApiResponse<UserResponse> getMyInfo(){
