c513d057480e1707e3a0d94937282a151f6af050
ducvu <vu99550@donga.edu.vn>
2025-03-31 21:36:51 +0700

    delete file never use

--- src/main/java/com/demo_crud/demo/repository/OrderItem/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo_crud.demo.repository.OrderItem;
-
-import com.demo_crud.demo.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, String> {
-}
